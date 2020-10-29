31a999264a0471749191e4019cc554b1384f5aaf
Thepetapixl <apurvvidhate40@gmail.com>
2020-10-29 18:44:29 +0530

    App connected to Firebase. Login is successful. Need to make the SignUp Page next

--- app/src/main/java/com/example/demologinsignup/MainActivity.java
+++ app/src/main/java/com/example/demologinsignup/MainActivity.java
@@ -1,28 +1,41 @@
 package com.example.demologinsignup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText TextEmail;
     private EditText TextPass;
-
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         Button btnLogin = findViewById(R.id.buttonLogin);
         Button btnClear = findViewById(R.id.buttonClear);
         TextEmail = findViewById(R.id.editTextTextEmailAddress);
         TextPass = findViewById(R.id.editTextTextPassword);
+
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +43,28 @@ public class MainActivity extends AppCompatActivity {
 
                 String login = TextEmail.getText().toString();
                 String password = TextPass.getText().toString();
+
+
+                mAuth.signInWithEmailAndPassword(login, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
 
@@ -44,5 +79,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void updateUI(FirebaseUser user) {
+
+        if(user == null){
+            Toast.makeText(MainActivity.this,"Authentication Failed", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(MainActivity.this,"User Authenticated", Toast.LENGTH_LONG).show();
+            String userId = user.getUid();
+        }
+
+    }
 
 }
